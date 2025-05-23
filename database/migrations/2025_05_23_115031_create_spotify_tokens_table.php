050710e0cdddb8b865f5efd517e7935aa7837b61
yoeehun <pantilgan.jonmarco30@gmail.com>
2025-05-23 20:17:45 +0800

    argh

--- database/migrations/2025_05_23_115031_create_spotify_tokens_table.php
+++ database/migrations/2025_05_23_115031_create_spotify_tokens_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('spotify_tokens', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('access_token');
+            $table->string('refresh_token')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
